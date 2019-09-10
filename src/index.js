b0fe8255b105838e718e2f42dbab8491225b232c
Val <valentinedestrait@hotmail.com>
2019-09-10 15:33:46 +0200

    Timeline

--- src/index.js
+++ src/index.js
@@ -2,3 +2,40 @@ import "./styles.scss"; // entry poin for styles
 import { test } from "./app/main" // example of an ES6 import
 
 console.log(test); // should display "it works!" in console on launch
+
+
+/* TIMELINE mvtsORDION */
+
+// Decade
+var dates = document.getElementsByClassName("date-heading");
+var i;
+
+for (i = 0; i < dates.length; i++) {
+  dates[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var decade = this.nextElementSibling;
+    if (decade.classList.contains("open")) {
+        decade.classList.remove("open");
+    } else {
+        decade.classList.add("open");
+    }
+  });
+} 
+
+// Movements
+var mvts = document.getElementsByClassName("mvt-heading");
+var i;
+
+for (i = 0; i < mvts.length; i++) {
+  mvts[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var mvtBody = this.nextElementSibling;
+    if (mvtBody.classList.contains("open")) {
+        mvtBody.classList.remove("open");
+    } else {
+        mvtBody.classList.add("open");
+    }
+  });
+} 
